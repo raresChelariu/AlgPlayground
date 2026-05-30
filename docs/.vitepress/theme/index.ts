db9eb30af2c654400b8510aceee209142f3887dd
Rares Chelariu <rareschelariu79@gmail.com>
2026-05-30 17:52:57 +0300

    chore: elimina componenta BinSearchPlot nefolosita
    Nicio lectie nu mai foloseste componenta, asa ca stergem inregistrarea
    din tema si fisierul .vue. VitePress revine la tema implicita.

--- docs/.vitepress/theme/index.ts
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from 'vitepress/theme'
-import BinSearchPlot from './components/BinSearchPlot.vue'
-
-export default {
-  extends: DefaultTheme,
-  enhanceApp({ app }) {
-    app.component('BinSearchPlot', BinSearchPlot)
-  },
-}
